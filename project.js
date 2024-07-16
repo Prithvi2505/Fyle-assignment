d13a1ee50878c98f371c383128d48e728f074667
Prithvi2505 <95611748+Prithvi2505@users.noreply.github.com>
2024-07-16 13:04:44 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+function changeImage1() {
+    var img = document.getElementById('image');
+    img.src = 'imgs/pimg1.png';
+}
+
+function changeImage2() {
+    var img = document.getElementById('image');
+    img.src = 'imgs/pimg2.jpg';
+}
+
+function changeImage3() {
+    var img = document.getElementById('image');
+    img.src = 'imgs/pimg3.jpg';
+}
